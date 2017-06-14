Updated plotting and fixed normalization.

ops/tf_model_cnn_joints.py
@@ -130,7 +130,7 @@ def train_and_eval(config):
                     labels=train_occlusions,
                     logits=model.fc8_occlusion))]
             loss_label += ['occlusion head']
-            loss = tf.add_n([loss_list[0]])
+            loss = tf.add_n(loss_list)
 
             # Add wd if necessary
             if config.wd_penalty is not None:
@@ -243,7 +243,7 @@ def train_and_eval(config):
             duration = time.time() - start_time
             assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
 
-            if step % 100 == 0 and step % 10 == 0:
+            if step % config.steps_before_validation == 0:
                 if validation_data is not False:
                     val_acc, val_pred, val_ims = sess.run(
                         [val_score, val_model.fc8, val_images])
@@ -272,30 +272,24 @@ def train_and_eval(config):
                 # Save the model checkpoint if it's the best yet
                 if config.normalize_labels:
                     normalize_vec = np.asarray(
-                        config.image_target_size[:2] + [config.max_depth]).repeat(
-                        len(config.joint_order))
+                        config.image_target_size[:2] + [config.max_depth]).reshape(
+                        1, -1).repeat(23, axis=0).reshape(1, -1)
                     yhat *= normalize_vec
                     ytrue *= normalize_vec
-
-                if step % 1000 == 0:
-                    np.save(
-                        os.path.join(results_dir, 'im_%s' % step), im)
-                    np.save(
-                        os.path.join(results_dir, 'yhat_%s' % step), yhat)
-                    np.save(
-                        os.path.join(results_dir, 'ytrue_%s' % step), ytrue)
-                    np.save(
-                        os.path.join(results_dir, 'occhat_%s' % step), occhat)
-                    np.save(
-                        os.path.join(results_dir, 'occtrue_%s' % step), occtrue)
-                    saver.save(
-                        sess, os.path.join(
-                            config.train_checkpoint,
-                            'model_' + str(step) + '.ckpt'), global_step=step)
-                    # model.save_npy(sess, npy_path=os.path.join(config.train_checkpoint, 'backup' + str(step)))
-
-                    # Store the new max validation accuracy
-                    # val_max = val_acc
+                np.save(
+                    os.path.join(results_dir, 'im_%s' % step), im)
+                np.save(
+                    os.path.join(results_dir, 'yhat_%s' % step), yhat)
+                np.save(
+                    os.path.join(results_dir, 'ytrue_%s' % step), ytrue)
+                np.save(
+                    os.path.join(results_dir, 'occhat_%s' % step), occhat)
+                np.save(
+                    os.path.join(results_dir, 'occtrue_%s' % step), occtrue)
+                saver.save(
+                    sess, os.path.join(
+                        config.train_checkpoint,
+                        'model_' + str(step) + '.ckpt'), global_step=step)
 
             else:
                 # Training status

visualization/monkey_mosaic.py
@@ -18,10 +18,11 @@ def save_mosaic(ims, yhats, ys, output):
         ax1.set_xticklabels([])
         ax1.set_yticklabels([])
         ax1.set_aspect('equal')
-        ax1.imshow(im)
-        import ipdb;ipdb.set_trace()
+        ax1.imshow(np.log10(im), cmap='Greys_r')
         plot_coordinates(ax1, y, 'r')
         plot_coordinates(ax1, yhat, 'b')
+    plt.subplots_adjust(top=1)
+    plt.suptitle('Blue = True Joint Position\nBlack = Predicted Joint Position.')
     plt.savefig(output)
 
 
@@ -44,16 +45,28 @@ def plot_coordinates(ax, vector, color):
 def main(
         num_files=1,
         output_file='monkey_mosaic.png',
-        monkey_dir='/media/data_cifs/monkey_tracking/batches/test/2017_06_13_18_14_22',
-        normalize=True
+        monkey_dir='/media/data_cifs/monkey_tracking/batches/test/2017_06_13_21_55_30',
+        normalize=False,
+        unnormalize=True
         ):
 
+    if unnormalize:
+        # Because normalization was fucked up in the training script
+        config = monkeyConfig()
+        unnormalize_vec = np.asarray(
+            config.image_target_size[:2] + [config.max_depth]).repeat(
+            len(config.joint_order))
+        normalize_vec = np.asarray(
+            config.image_target_size[:2] + [config.max_depth]).reshape(
+            1, -1).repeat(23, axis=0).reshape(1, -1)
     if normalize:
         config = monkeyConfig()
         normalize_vec = np.asarray(
-            config.image_target_size[:2] + [config.max_depth]).repeat(
-            len(config.joint_order))
-    ims = sorted(glob(os.path.join(monkey_dir, 'im_*')))[::-1][:num_files]
+            config.image_target_size[:2] + [config.max_depth]).reshape(
+            1, -1).repeat(23, axis=0).reshape(1, -1)
+    ims = glob(os.path.join(monkey_dir, 'im_*'))
+    ims.sort(key=os.path.getmtime)
+    ims = ims[::-1][:num_files]
     im_list = []
     yhat_list = []
     ytrue_list = []
@@ -63,6 +76,11 @@ def main(
         yhats = np.load(os.path.join(monkey_dir, 'yhat_%s.npy' % file_name))
         ytrues = np.load(os.path.join(monkey_dir, 'ytrue_%s.npy' % file_name))
         if normalize:
+            yhats *= normalize_vec
+            ytrues *= normalize_vec
+        if unnormalize:
+            yhats /= unnormalize_vec
+            ytrues /= unnormalize_vec
             yhats *= normalize_vec
             ytrues *= normalize_vec
         [im_list.append(x) for x in images]