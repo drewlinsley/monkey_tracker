Fixed aux loss masking. Fixed encoding of dummy aux losses for kinect data.

ops/convert_babas_annotations_to_tfrecords.py
@@ -21,6 +21,7 @@ def main(tmp_folder='tmp'):
                 project_dir = os.path.join(tmp_folder, batch['data'])
                 im_folder = os.path.join(project_dir, 'images')
                 label_folder = os.path.join(project_dir, 'labels')
+                occlusion_folder = os.path.join(project_dir, 'occlusion')
                 [tf_fun.make_dir(x) for x in [
                     tmp_folder,
                     project_dir,
@@ -55,6 +56,8 @@ def main(tmp_folder='tmp'):
             fnames, flat_ims)]
         [np.save(os.path.join(label_folder, f), im) for f, im in zip(
             fnames, annotations)]
+        [np.save(os.path.join(occlusion_folder, f), im) for f, im in zip(
+            fnames, np.zeros((len(config.joint_names))))]
         print 'Found %s annotations, %s images.' % (
             len(annotations),
             len(flat_ims))
@@ -62,8 +65,8 @@ def main(tmp_folder='tmp'):
         # Set data folders in config
         config.depth_dir = im_folder
         config.label_dir = label_folder
-        config.pixel_label_dir = label_folder
-        config.occlusion_dir = label_folder
+        config.pixel_label_dir = im_folder
+        config.occlusion_dir = occlusion_folder
         config.tfrecord_dir = '/media/data_cifs/monkey_tracking/data_for_babas/tfrecords_from_babas'
         config.use_train_as_val = True
         process_data(config)
@@ -76,3 +79,4 @@ def main(tmp_folder='tmp'):
 
 if __name__ == '__main__':
     main()
+

ops/loss_helper.py
@@ -95,15 +95,19 @@ def get_aux_losses(
     aux_dict = aux_loss_dict.values()[0]
     if aux_loss_dict.keys()[0] in train_data_dict.keys():
         output_label = aux_dict['var_label']
-        if domain_adaptation and aux_dict['da_override']:
-            import ipdb;ipdb.set_trace()
-            loss_mask = 0.  # MASK OUT ALL 0s IN THE LOSS
-
         y = train_data_dict[aux_dict['y_name']]
         yhat = model[aux_dict['model_name']]
         loss_function = aux_dict['loss_function']
         reg_weight = aux_dict['lambda']
         aux_fun = aux_dict['aux_fun']
+        if domain_adaptation and aux_dict['da_override']:
+            loss_mask = tf.expand_dims(tf.cast(
+                tf.equal(
+                    train_data_dict['domain_adaptation'][:, 0],
+                    0),
+                tf.float32), axis=1)  # Mask out the kinect data (first column)
+        else:
+            loss_mask = tf.ones(int(yhat.get_shape()[0]))
         if aux_fun == 'resize':
             y = tf.image.resize_bilinear(
                 y, [int(x) for x in yhat.get_shape()[1:3]])
@@ -111,7 +115,7 @@ def get_aux_losses(
             loss = tf.reduce_mean(
                 tf.nn.sigmoid_cross_entropy_with_logits(
                     labels=y,
-                    logits=yhat))
+                    logits=yhat) * loss_mask)
         elif loss_function == 'cce':
             if isinstance(reg_weight, dict):
                 # Only using index 1 for now
@@ -126,15 +130,15 @@ def get_aux_losses(
                 inter_loss = tf.nn.softmax_cross_entropy_with_logits(
                     logits=yhat,
                     labels=y,
-                    dim=-1)
+                    dim=-1) * loss_mask
                 loss = tf.reduce_mean(inter_loss * weights)
             else:
                 loss = tf.reduce_mean(
                     tf.nn.softmax_cross_entropy_with_logits(
                         labels=tf.cast(tf.squeeze(y), tf.int32),
-                        logits=yhat))
+                        logits=yhat) * loss_mask)
         elif loss_function == 'l2':
-            loss = tf.nn.l2_loss(y - yhat)
+            loss = tf.nn.l2_loss((y - yhat) * loss_mask)
         if reg_weight is not None and not isinstance(reg_weight, dict):
             loss *= reg_weight
         loss_list += [loss]

ops/tf_model_cnn_joints.py
@@ -74,6 +74,9 @@ def train_and_eval(config, babas_data):
 	train_babas_tfrecord_dir = os.path.join(config.babas_tfrecord_dir, config.train_tfrecords)
         if config.include_validation:
             val_babas_tfrecord_dir = os.path.join(config.babas_tfrecord_dir, config.val_tfrecords)
+    else:
+        train_babas_tfrecord_dir = None
+        val_babas_tfrecord_dir = None
 
     if isinstance(config.include_validation, basestring):
         validation_data = config.include_validation
@@ -227,7 +230,8 @@ def train_and_eval(config, babas_data):
                     loss_label=loss_label,
                     train_data_dict=train_data_dict,
                     model=model,
-                    aux_loss_dict=al)
+                    aux_loss_dict=al,
+                    domain_adaptation=train_babas_tfrecord_dir)
             loss = tf.add_n(loss_list)
 
             # Add wd if necessary